8a93a6f04183741da84d3c03b63da7ee7556382f
wtmsys <wtmsystems@gmail.com>
2022-04-09 20:56:43 +0700

    Initialize hms-activity2

---
+++ src/pages/conference.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useContext, useState, useCallback } from "react";
+import { AppContext } from "../store/AppContext";
+import { useHistory, useParams } from "react-router-dom";
+import { ConferenceHeader } from "../views/headerView";
+import { ConferenceFooter } from "../views/footerView";
+import { ConferenceMainView } from "../views/mainView";
+import { Notifications } from "../views/components/notifications";
+
+export const Conference = () => {
+  const history = useHistory();
+  const { roomId, role } = useParams();
+  const context = useContext(AppContext);
+  const [isChatOpen, setIsChatOpen] = useState(false);
+  const [isParticipantListOpen, setIsParticipantListOpen] = useState(false);
+  const toggleChat = useCallback(() => {
+    setIsChatOpen(open => !open);
+  }, []);
+
+  const onParticipantListOpen = useCallback(value => {
+    setIsParticipantListOpen(value);
+  }, []);
+
+  const { loginInfo, leave } = context;
+
+  useEffect(() => {
+    if (!roomId) {
+      history.push(`/`);
+    }
+    if (!loginInfo.token) {
+      // redirect to join if token not present
+      if (role) history.push(`/preview/${loginInfo.roomId || roomId || ""}/${role}`);
+      else history.push(`/preview/${loginInfo.roomId || roomId || ""}`);
+    }
+
+    return () => {
+      leave();
+    };
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className="w-full h-full flex flex-col dark:bg-black">
+      <div className="h-14 md:h-16">
+        <ConferenceHeader onParticipantListOpen={onParticipantListOpen} />
+      </div>
+      <div className="w-full flex flex-1 flex-col md:flex-row">
+        <ConferenceMainView
+          isChatOpen={isChatOpen}
+          isParticipantListOpen={isParticipantListOpen}
+          toggleChat={toggleChat}
+        />
+        <Notifications />
+      </div>
+      <div className="dark:bg-black" style={{ height: "10%" }}>
+        <ConferenceFooter isChatOpen={isChatOpen} toggleChat={toggleChat} />
+      </div>
+    </div>
+  );
+};
+
---
+++ src/views/footerView.jsx
@@ -0,0 +1,283 @@
+import React, {
+  useState,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+} from "react";
+import {
+  useHMSStore,
+  ControlBar,
+  HangUpIcon,
+  MicOffIcon,
+  MicOnIcon,
+  CamOffIcon,
+  CamOnIcon,
+  VirtualBackgroundIcon,
+  Button,
+  ShareScreenIcon,
+  ChatIcon,
+  ChatUnreadIcon,
+  VerticalDivider,
+  MessageModal,
+  useHMSActions,
+  Settings,
+  selectIsLocalScreenShared,
+  selectIsLocalAudioEnabled,
+  selectIsLocalVideoDisplayEnabled,
+  selectUnreadHMSMessagesCount,
+  selectLocalMediaSettings,
+  isMobileDevice,
+} from "@100mslive/hms-video-react";
+import { useHistory, useParams } from "react-router-dom";
+import { HMSVirtualBackgroundPlugin } from "@100mslive/hms-virtual-background";
+import { HMSParticipationMetricsPlugin } from "@100mslive/hms-faceapi";
+import { AppContext } from "../store/AppContext";
+
+const SettingsView = () => {
+  const hmsActions = useHMSActions();
+  const { setMaxTileCount } = useContext(AppContext);
+  const {
+    audioInputDeviceId,
+    videoInputDeviceId,
+    audioOutputDeviceId,
+  } = useHMSStore(selectLocalMediaSettings);
+
+  const onChange = ({
+    maxTileCount: newMaxTileCount,
+    selectedVideoInput: newSelectedVideoInput,
+    selectedAudioInput: newSelectedAudioInput,
+    selectedAudioOutput: newSelectedAudioOuput,
+  }) => {
+    setMaxTileCount(newMaxTileCount);
+    if (audioInputDeviceId !== newSelectedAudioInput) {
+      hmsActions.setAudioSettings({ deviceId: newSelectedAudioInput });
+    }
+
+    if (videoInputDeviceId !== newSelectedVideoInput) {
+      hmsActions.setVideoSettings({ deviceId: newSelectedVideoInput });
+    }
+
+    if (audioOutputDeviceId !== newSelectedAudioOuput) {
+      hmsActions.setAudioOutputDevice(newSelectedAudioOuput);
+    }
+  };
+  return (
+    <>
+      <Settings
+        onChange={onChange}
+        initialValues={{
+          selectedAudioInput: audioInputDeviceId,
+          selectedVideoInput: videoInputDeviceId,
+        }}
+        classes={{ sliderContainer: "hidden md:block", root: "mr-2 md:mr-0" }}
+      />
+    </>
+  );
+};
+
+export const ConferenceFooter = ({ isChatOpen, toggleChat }) => {
+  const isScreenShared = useHMSStore(selectIsLocalScreenShared);
+  const isLocalAudioEnabled = useHMSStore(selectIsLocalAudioEnabled);
+  const isLocalVideoEnabled = useHMSStore(selectIsLocalVideoDisplayEnabled);
+  const countUnreadMessages = useHMSStore(selectUnreadHMSMessagesCount);
+  const hmsActions = useHMSActions();
+  const { isConnected, leave } = useContext(AppContext);
+  const [showBackground, setShowBackground] = useState(false);
+  const [showDetection, setShowDetection] = useState(false);
+  const history = useHistory();
+  const params = useParams();
+  const pluginRef = useRef(null);
+  const plugin2Ref = useRef(null);
+
+  const initialModalProps = {
+    show: false,
+    title: "",
+    body: "",
+  };
+  const [errorModal, setErrorModal] = useState(initialModalProps);
+
+  useEffect(() => {
+    async function startPlugin() {
+      if (!pluginRef.current) {
+        pluginRef.current = new HMSVirtualBackgroundPlugin('blur');
+      }
+      await hmsActions.addPluginToVideoTrack(pluginRef.current);
+    }
+    async function removePlugin() {
+      if (pluginRef.current) {
+        await hmsActions.removePluginFromVideoTrack(pluginRef.current);
+      }
+    }
+    if (showBackground) {
+      startPlugin();
+    } else {
+      removePlugin();
+    }
+  }, [showBackground]); //eslint-disable-line
+
+
+   useEffect(() => {
+    async function startPlugin() {
+      if (!plugin2Ref.current) {
+        plugin2Ref.current = new HMSParticipationMetricsPlugin(true,false,0.1,true,true);
+    }
+      await hmsActions.addPluginToVideoTrack(plugin2Ref.current);
+    }
+    async function removePlugin() {
+     if (plugin2Ref.current) {
+        await hmsActions.removePluginFromVideoTrack(plugin2Ref.current);
+     }
+    }
+    if (showDetection) {
+      startPlugin();
+    } else {
+      removePlugin();
+    }
+  }, [showDetection]); //eslint-disable-line
+
+
+  const toggleAudio = useCallback(async () => {
+    try {
+      await hmsActions.setLocalAudioEnabled(!isLocalAudioEnabled);
+    } catch (err) {
+      console.error("Cannot toggle audio", err);
+    }
+  }, [hmsActions, isLocalAudioEnabled]);
+
+  const toggleVideo = useCallback(async () => {
+    try {
+      await hmsActions.setLocalVideoEnabled(!isLocalVideoEnabled);
+    } catch (err) {
+      console.error("Cannot toggle video", err);
+    }
+  }, [hmsActions, isLocalVideoEnabled]);
+
+  const toggleScreenShare = useCallback(async () => {
+    try {
+      await hmsActions.setScreenShareEnabled(!isScreenShared);
+    } catch (error) {
+      if (error.description.includes("denied by system")) {
+        setErrorModal({
+          show: true,
+          title: "Screen share permission denied by OS",
+          body: "Please update your OS settings to permit screen share.",
+        });
+      }
+    }
+  }, [hmsActions, isScreenShared]);
+
+  const leftComponents = [<SettingsView key={0} />];
+
+  if (!isMobileDevice()) {
+    leftComponents.push(
+      ...[
+        <VerticalDivider key={1} />,
+        <Button
+          key={2}
+          iconOnly
+          variant="no-fill"
+          iconSize="md"
+          shape="rectangle"
+          onClick={toggleScreenShare}
+        >
+          <ShareScreenIcon />
+        </Button>,
+        <VerticalDivider key={3} />,
+        <Button
+          key={4}
+          iconOnly
+          variant="no-fill"
+          iconSize="md"
+          shape="rectangle"
+          onClick={toggleChat}
+          active={isChatOpen}
+        >
+          {countUnreadMessages === 0 ? <ChatIcon /> : <ChatUnreadIcon />}
+        </Button>,
+      ]
+    );
+  }
+
+  return isConnected ? (
+    <>
+      <ControlBar
+        leftComponents={leftComponents}
+        centerComponents={[
+          <Button
+            iconOnly
+            variant="no-fill"
+            iconSize="md"
+            classes={{ root: "mr-2" }}
+            shape="rectangle"
+            active={!isLocalAudioEnabled}
+            onClick={toggleAudio}
+            key={0}
+          >
+            {!isLocalAudioEnabled ? <MicOffIcon /> : <MicOnIcon />}
+          </Button>,
+          <Button
+            iconOnly
+            variant="no-fill"
+            iconSize="md"
+            classes={{ root: "mr-2" }}
+            shape="rectangle"
+            active={!isLocalVideoEnabled}
+            onClick={toggleVideo}
+            key={1}
+          >
+            {!isLocalVideoEnabled ? <CamOffIcon /> : <CamOnIcon />}
+          </Button>,
+          <Button
+            iconOnly
+            variant="no-fill"
+            shape="rectangle"
+            active={showBackground}
+            onClick={() => setShowBackground(!showBackground)}
+            key={2}
+          >
+            <VirtualBackgroundIcon />
+          </Button>,
+           <Button
+            iconOnly
+            variant="no-fill"
+            shape="rectangle"
+            active={showDetection}
+            onClick={() => setShowDetection(!showDetection)}
+            key={3}
+          >
+            <VirtualBackgroundIcon />
+          </Button>
+        ]}
+        rightComponents={[
+          <Button
+            key={0}
+            size="md"
+            shape="rectangle"
+            variant="danger"
+            onClick={() => {
+              leave();
+              if (params.role)
+                history.push("/leave/" + params.roomId + "/" + params.role);
+              else history.push("/leave/" + params.roomId);
+            }}
+          >
+            <HangUpIcon className="mr-2" />
+            Leave room
+          </Button>,
+        ]}
+        audioButtonOnClick={toggleAudio}
+        videoButtonOnClick={toggleVideo}
+        backgroundButtonOnClick={() => setShowBackground(!showBackground)}
+        isAudioMuted={!isLocalAudioEnabled}
+        isVideoMuted={!isLocalVideoEnabled}
+        isBackgroundEnabled={showBackground}
+        isDetectionEnabled = {showDetection}
+      />
+      <MessageModal
+        {...errorModal}
+        onClose={() => setErrorModal(initialModalProps)}
+      />
+    </>
+  ) : null;
+};
